29deb7c09e01e87c831a8dadaac5db45f48146b3
Lydia Wildes <lydiawildes1@gmail.com>
2022-11-14 20:40:49 -0600

    create international/domestic checkbox component

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
+// import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import { useState } from 'react';
+
+// import MUI elements
+
+//import components
+import International from '../International/International';
 
 function UserPage() {
+
+
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
     <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+      <h2>welcome, {user.username}</h2>
+      <h3>let's dream!</h3>
+      {/* <p>Your ID is: {user.id}</p> */}
+      {/* <LogOutButton className="btn" /> */}
+
+      <div>
+        <h5>selected feature chips will appear here</h5>
+        {/* Display chips by rendering an array of chips */}
+      </div>
+      <div>
+        <International />
+      </div>
+
     </div>
   );
 }
