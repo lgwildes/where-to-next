f3f9fe026c62cf2aa2b1ec4735e944b3dab498cf
Lydia Wildes <lydiawildes1@gmail.com>
2022-11-16 10:50:14 -0600

    create function to set variable placeholders in sql query and get results from database

--- server/routes/submit.router.js
+++ server/routes/submit.router.js
@@ -13,6 +13,58 @@ router.get('/', rejectUnauthenticated, (req, res) => {
   // GET route code here
   let preferences = req.query
   console.log('submit.router GET preferences', preferences)
+
+    //TODO add IF statement for international/domestic
+  
+    // loop through preferences.features to find number of parameters.
+    // set variable placeholders $1, $2, etc. to the number of features
+   function countPlaceholders() {
+        
+        let placeholders = ``
+        for (let i = 0; i < preferences.features.length; i++) {
+           
+            // for each id EXCEPT the last, add a comma after 
+            if(i < preferences.features.length-1){
+                placeholders += ` $${i+1},`
+            }
+            else {
+                placeholders += ` $${i+1}`
+            }
+           
+            // console.log('TESTING OUT placeholders 🚙', placeholders)
+            
+        }
+        return placeholders
+    }
+
+    let variablePlaceholders = countPlaceholders();
+
+    // console.log('🥳variablePlaceholders to insert into query', variablePlaceholders)
+
+    // sqlText to insert into query with variable placeholders for feature ids
+    let sqlText =  `SELECT "destination"."id", "name", "international", json_agg(feature_id) AS features, "description" FROM "destination" 
+    JOIN destination_feature ON destination_id = destination.id
+    WHERE feature_id IN(${variablePlaceholders})
+    GROUP BY "destination"."id"
+    ORDER BY "destination".id
+    ;`
+
+    console.log('My sqlText is...', sqlText);
+    
+    // params for the query!
+    let sqlParams = preferences.features;
+    console.log('SQL PARAMS ARE', sqlParams)
+
+    pool.query(sqlText, sqlParams)
+        .then( dbResult => {
+            res.send(dbResult.rows);
+            console.log('😱response from database is', dbResult.rows)
+        })
+        .catch( error => {
+            console.log('error in submit.router GET', error)
+            res.sendStatus(500);
+        })
+
      
 });
 
